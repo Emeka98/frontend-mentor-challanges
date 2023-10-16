5ce43b04c7ac45656e3026927c6f78b84842a908
Guney Unay <guneymarketing17@hotmail.com>
2023-10-16 18:15:39 +0300

    design is done for all devices screen

--- launch-countdownt-timer/tailwind.config.js
+++ launch-countdownt-timer/tailwind.config.js
@@ -3,7 +3,15 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
 
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        "red-head": ["Red Hat Text", "sans-serif"],
+      },
+      backgroundImage:{
+        'star' : 'url(/src/assets/bg-stars.svg)',
+        'hills' : 'url(/src/assets/pattern-hills.svg)'
+      }
+    },
   },
   plugins: [],
 };
