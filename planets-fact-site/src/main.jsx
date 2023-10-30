dca17314a358eecd407f97a642c3bfb840664f3b
Guney Unay <guneymarketing17@hotmail.com>
2023-10-30 18:45:48 +0300

    added context api

--- planets-fact-site/src/main.jsx
+++ planets-fact-site/src/main.jsx
@@ -2,9 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import { DataContextProvider } from "./context/DataContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
+  <DataContextProvider>
     <App />
-  </React.StrictMode>
+  </DataContextProvider>
 );
