1791e8c6f0a9f0c6bda03fe2ca6fb8a98e3babf0
Guney Unay <guneymarketing17@hotmail.com>
2023-11-01 17:09:16 +0300

    added context api

--- in-browser-markdown-editor/src/App.jsx
+++ in-browser-markdown-editor/src/App.jsx
@@ -1,4 +1,8 @@
+import { useData } from "./context/DataContext";
+
 function App() {
+  const { theme } = useData();
+  console.log(theme)
   return (
     <>
       <p className="text-red-500">Test</p>
---
+++ in-browser-markdown-editor/src/context/DataContext.jsx
@@ -0,0 +1,26 @@
+import React, { createContext, useContext, useState , useMemo} from "react";
+
+const DataContext = createContext();
+
+export function DataContextProvider({ children }) {
+  const [theme, setTheme] = useState(false);
+  const contextData = useMemo(
+    () => ({
+      theme,
+      setTheme,
+    }),
+    [theme]
+  );
+
+  return (
+    <DataContext.Provider value={contextData}>{children}</DataContext.Provider>
+  );
+}
+
+export function useData() {
+  const context = useContext(DataContext);
+  if (!context) {
+    throw new Error("There is no context!");
+  }
+  return context;
+}
