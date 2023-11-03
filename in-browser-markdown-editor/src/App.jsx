61f76ed7cd91dcb48fd16d0575c461a5000fc199
Guney Unay <guneymarketing17@hotmail.com>
2023-11-03 18:55:40 +0300

    fixed some minor bugs

--- in-browser-markdown-editor/src/App.jsx
+++ in-browser-markdown-editor/src/App.jsx
@@ -2,10 +2,10 @@ import Navbar from "./components/Navbar";
 import AsideBar from "./components/AsideBar";
 function App() {
   return (
-    <>
+    <div className="min-h-full w-screen overflow-hidden">
       <AsideBar />
       <Navbar />
-    </>
+    </div>
   );
 }
 
