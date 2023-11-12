21fff8b1639a219762baf649a63662e44ef318e8
Guney Unay <guneymarketing17@hotmail.com>
2023-11-12 18:50:49 +0300

    added head.js

---
+++ frontend-quiz-app/app/head.js
@@ -0,0 +1,10 @@
+function RootHead() {
+  return (
+    <>
+      <title>Frontend Quiz App</title>
+      <link rel="shortcut icon" href="@/public/favicon-32x32.png" />
+    </>
+  );
+}
+
+export default RootHead;
