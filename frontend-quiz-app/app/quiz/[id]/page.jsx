e062cc9dc06b7616385b180abfeee95b7150c8f4
Guney Unay <guneymarketing17@hotmail.com>
2023-11-13 00:42:08 +0300

    fetching process..

--- frontend-quiz-app/app/quiz/[id]/page.jsx
+++ frontend-quiz-app/app/quiz/[id]/page.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 
 const getData = async () => {
-  fetch(`https://${process.env.API_KEY}.mockapi.io/api/v1/quizzes`)
-    .then((res) => res.json())
-    .then((data) => console.log(data));
+  const res = await fetch(
+    `https://${process.env.API_KEY}.mockapi.io/api/v1/quizzes`
+  );
+  return res.json()
 };
 
 async function QuizPage({ params }) {
-  await getData();
+  const data = await getData();
+  console.log(data[0].quizzes[0]);
   const { id } = params;
 
   return <div className="lg:container lg:mx-auto">Quiz Page</div>;
