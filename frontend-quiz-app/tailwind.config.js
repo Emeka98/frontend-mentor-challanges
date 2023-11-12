7b92a5f93aea3f053881ddb260bdf90f256a393e
Guney Unay <guneymarketing17@hotmail.com>
2023-11-12 13:03:06 +0300

    added colors 🎨

--- frontend-quiz-app/tailwind.config.js
+++ frontend-quiz-app/tailwind.config.js
@@ -1,18 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        'purple': "#A729F5",
+        "dark-navy": "#313E51",
+        'navy': "#3B4D66",
+        "grey-navy": "#626C7F",
+        "light-bluish": "#ABC1E1",
+        "light-grey": "#F4F6FA",
+        'green': "#26D782",
+        'red': "#EE5454",
       },
     },
   },
   plugins: [],
-}
+};
