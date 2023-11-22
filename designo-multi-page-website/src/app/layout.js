9a7b34b855f5022bbe4525ae7e78f51a11888081
Guney Unay <guneymarketing17@hotmail.com>
2023-11-22 19:11:27 +0300

    added font

--- designo-multi-page-website/src/app/layout.js
+++ designo-multi-page-website/src/app/layout.js
@@ -1,8 +1,11 @@
 import "./globals.css";
+import { Jost } from "next/font/google";
+
+const jost = Jost({ subsets: ["latin"] });
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" className={jost.className}>
       <body>{children}</body>
     </html>
   );
