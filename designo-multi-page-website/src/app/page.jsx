cca92e2a26056d4c1b8b78215a28aba2ddefee27
Guney Unay <guneymarketing17@hotmail.com>
2023-11-23 12:59:08 +0300

    added containers

--- designo-multi-page-website/src/app/page.jsx
+++ designo-multi-page-website/src/app/page.jsx
@@ -1,18 +1,12 @@
+import HomeContainer from "@/containers/HomeContainer";
+
 export const metadata = {
-  title: "Homepage",
+  title: "Designo | Homepage",
   description: "Designo homepage",
 };
 
 export default function Home() {
   return (
-    <div>
-      <h1 className="heading-l">Deneme</h1>
-      <h2 className="heading-m">Deneme 2</h2>
-      <h3 className="heading-s">Deneme 3</h3>
-      <p className="custom-text">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga,
-        repudiandae!
-      </p>
-    </div>
+<HomeContainer />
   );
 }
