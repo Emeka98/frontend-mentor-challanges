209d1661b3123bb3c66b478f32f11ead30b9ac0e
Guney Unay <guneymarketing17@hotmail.com>
2023-11-27 18:28:54 +0300

    fixed some styles bugs

--- designo-multi-page-website/components/Maps/index.jsx
+++ designo-multi-page-website/components/Maps/index.jsx
@@ -5,19 +5,17 @@ import { MapContainer, TileLayer } from "react-leaflet";
 
 function Maps() {
   return (
-    <div className="w-full h-[320px] lg:max-w-[350px] ">
-      <MapContainer
-        className="w-full h-full md:rounded-[15px]"
-        center={[43.662299, -79.314497]}
-        zoom={15}
-        scrollWheelZoom={false}
-      >
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-      </MapContainer>
-    </div>
+    <MapContainer
+      className="w-full h-full md:rounded-[15px] z-20"
+      center={[43.662299, -79.314497]}
+      zoom={15}
+      scrollWheelZoom={false}
+    >
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+    </MapContainer>
   );
 }
 
