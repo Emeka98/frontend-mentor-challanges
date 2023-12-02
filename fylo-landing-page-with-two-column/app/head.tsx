f43eb1a9081aad673be165f09306fbc3cca28a41
Guney Unay <guneymarketing17@hotmail.com>
2023-12-02 19:32:41 +0300

    added head component

---
+++ fylo-landing-page-with-two-column/app/head.tsx
@@ -0,0 +1,9 @@
+export default function Head() {
+  return (
+    <head>
+      <title>Fylo Landing Page</title>
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta name="description" content="Fylo landing page." />
+    </head>
+  );
+}
