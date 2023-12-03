af04c040538ffc641a7446d358a578aedfd0655f
Guney Unay <guneymarketing17@hotmail.com>
2023-12-04 00:00:05 +0300

    added section two

--- fylo-landing-page-with-two-column/components/MainContent/index.tsx
+++ fylo-landing-page-with-two-column/components/MainContent/index.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import section1Img from "@/assets/icons/illustration-1.svg?url";
 import section2Img from "@/assets/icons/illustration-2.svg?url";
-
+import Link from "next/link";
 import Image from "next/image";
 import Input from "../Input";
 import Button from "../Button";
+import IconArrow from "@/assets/icons/icon-arrow.svg";
+import IconQuotes from "@/assets/icons/icon-quotes.svg";
+import avatarImg from "@/assets/avatar-testimonial.jpg";
 
 function MainContent() {
   return (
@@ -30,10 +33,59 @@ function MainContent() {
         </div>
       </section>
 
-      <section className="flex flex-col gap-10 mt-[100px] md:mt-[180px] bg-[#F8F8FE] relative pt-[90px]">
+      <section className="flex flex-col gap-10 mt-[100px] md:mt-[180px] bg-[#F8F8FE] relative py-[90px] px-6">
         <div className="absolute -top-12 left-0 right-0 bottom-0 bg-curve-mobile md:bg-curve-desktop bg-no-repeat bg-contain z-10"></div>
         <div className=" relative w-full h-[225px] ">
           <Image src={section2Img} alt="Section two image" fill />
+        </div>
+        <div className="flex flex-col gap-6">
+          <h3 className="text-[#07043B] text-center  font-semibold text-base leading-6">
+            Stay productive, wherever you are
+          </h3>
+          <div className="flex flex-col gap-4 items-center ">
+            <p className="text-[#615f7e] text-center text-sm leading-5">
+              Never let location be an issue when accessing your files. Fylo has
+              you covered for all of your file storage needs.
+            </p>
+            <p className="text-[#615f7e] text-center text-sm leading-5">
+              Securely share files and folders with friends, family and
+              colleagues for live collaboration. No email attachments required!
+            </p>
+
+            <Link
+              href="/"
+              className="inline-flex items-center gap-2  border-b border-solid border-[#3da08f] pb-1.5  "
+            >
+              <p className="text-[#3da08f] text-xs leading-normal font-normal ">
+                See how Fylo Works
+              </p>
+              <IconArrow />
+            </Link>
+          </div>
+
+          <div className="bg-white px-6 mx-6 py-5 mt-10">
+            <div className="flex flex-col gap-3 ">
+              <IconQuotes />
+              <p className="text-[#07043b] text-[10px] leading-[18px] tracking-[0.385px] ">
+                Fylo has improved our team productivity by an order of
+                magnitude. Since making the switch our team has become a
+                well-oiled collaboration machine.
+              </p>
+              <div className="flex gap-2">
+                <div className="w-6 h-6 rounded-full overflow-hidden relative">
+                  <Image src={avatarImg} alt="avatar's of kyle burton" fill />
+                </div>
+                <div className="flex flex-col gap-0.5">
+                  <h4 className="text-[9px] tracking-[0.45px] text-[#07043b]">
+                    Kyle Burton
+                  </h4>
+                  <p className="text-[#07043b] text-[6px] leading-3 tracking-[0.3px]">
+                    Founder & CEO , Huddle
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </section>
     </main>
